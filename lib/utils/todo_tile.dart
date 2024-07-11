3992de3803ccf6f163011d3623ca7c62b1352574
Ibn Masood <syed.rayyan.masood@gmail.com>
2024-07-11 16:38:48 +0500

    Added a delete slidable button, no functionality yet

--- lib/utils/todo_tile.dart
+++ lib/utils/todo_tile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoTile extends StatelessWidget {
     final String taskName;
@@ -16,30 +18,41 @@ class TodoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right:20.0, left:20.0, top:20.0),
-      child: Container(
-        padding: EdgeInsets.all(18),
-        // color: Colors.amber[400],
-        decoration: BoxDecoration(
-            color: Colors.amber[100],
-            borderRadius: BorderRadius.circular(15),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            // add checkbox to the left
-            Checkbox(
-              value: taskCompleted, 
-              onChanged:onChanged,
-              activeColor: Colors.black,
-            ),
-
-            // Task name
-            Text(
-              taskName,
-              style: TextStyle(
-                decoration: taskCompleted ? TextDecoration.lineThrough : TextDecoration.none,
-                fontSize: 20),
-            ),
+            SlidableAction(onPressed:(context) {},
+            icon: Icons.delete,
+            backgroundColor: Colors.red,
+            borderRadius: BorderRadius.only(topRight: Radius.circular(15), bottomRight: Radius.circular(15)),),
           ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(18),
+          // color: Colors.amber[400],
+          decoration: BoxDecoration(
+              color: Colors.amber[100],
+              borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            children: [
+              // add checkbox to the left
+              Checkbox(
+                value: taskCompleted, 
+                onChanged:onChanged,
+                activeColor: Colors.black,
+              ),
+        
+              // Task name
+              Text(
+                taskName,
+                style: TextStyle(
+                  decoration: taskCompleted ? TextDecoration.lineThrough : TextDecoration.none,
+                  fontSize: 20),
+              ),
+            ],
+          ),
         ),
       ),
 
