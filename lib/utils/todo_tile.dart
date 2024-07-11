ba664e4b57d54123a87938e54d17ac1027504196
Ibn Masood <syed.rayyan.masood@gmail.com>
2024-07-11 17:54:51 +0500

    Made it so that the slidable buttons properly "merge" with the rounded corners

--- lib/utils/todo_tile.dart
+++ lib/utils/todo_tile.dart
@@ -3,59 +3,82 @@ import 'package:flutter/services.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoTile extends StatelessWidget {
-    final String taskName;
-    final bool taskCompleted;
-    final Function(bool?)? onChanged;
+  final String taskName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
 
-  TodoTile({
-    super.key,
-    required this.taskName, 
-    required this.taskCompleted,
-    required this.onChanged
-  });
-  
+  TodoTile(
+      {super.key,
+      required this.taskName,
+      required this.taskCompleted,
+      required this.onChanged});
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right:20.0, left:20.0, top:20.0),
-      child: Slidable(
-        endActionPane: ActionPane(
-          motion: StretchMotion(),
-          children: [
-            SlidableAction(onPressed:(context) {},
-            icon: Icons.delete,
-            backgroundColor: Colors.red,
-            borderRadius: BorderRadius.only(topRight: Radius.circular(15), bottomRight: Radius.circular(15)),),
-          ],
-        ),
-        child: Container(
-          padding: EdgeInsets.all(18),
-          // color: Colors.amber[400],
-          decoration: BoxDecoration(
-              color: Colors.amber[100],
-              borderRadius: BorderRadius.circular(15),
-          ),
-          child: Row(
-            children: [
-              // add checkbox to the left
-              Checkbox(
-                value: taskCompleted, 
-                onChanged:onChanged,
-                activeColor: Colors.black,
+      padding: const EdgeInsets.only(right: 20.0, left: 20.0, top: 20.0),
+      child: Stack(
+        clipBehavior: Clip.antiAlias,
+        children: [
+          Positioned.fill(
+            child: Builder(
+              builder: (context) => Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(
+                        15), // Make sure this matches your child container's radius
+                  ),
+                ),
               ),
-        
-              // Task name
-              Text(
-                taskName,
-                style: TextStyle(
-                  decoration: taskCompleted ? TextDecoration.lineThrough : TextDecoration.none,
-                  fontSize: 20),
-              ),
-            ],
+            ),
           ),
-        ),
+          Slidable(
+            key: UniqueKey(),
+            endActionPane: ActionPane(
+              motion: StretchMotion(),
+              extentRatio: 0.3,
+              children: [
+                SlidableAction(
+                  onPressed: (context) {},
+                  icon: Icons.delete,
+                  backgroundColor: Colors.red,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(15),
+                    bottomRight: Radius.circular(15),
+                  ),
+                ),
+              ],
+            ),
+            child: Container(
+              padding: EdgeInsets.all(18),
+              decoration: BoxDecoration(
+                color: Colors.amber[100],
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Row(
+                children: [
+                  Checkbox(
+                    value: taskCompleted,
+                    onChanged: onChanged,
+                    activeColor: Colors.black,
+                  ),
+                  Text(
+                    taskName,
+                    style: TextStyle(
+                      decoration: taskCompleted
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-
     );
   }
 }
